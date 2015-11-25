ab0cbb56aca6674099f8b5fa73d9942518c2096b
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-24 23:32:12 -0500

    Adding functionality for authentication

--- Shopping/Data/RepositoryMySql.cs
+++ Shopping/Data/RepositoryMySql.cs
@@ -1,35 +1,154 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Shopping.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace Shopping.Data
 {
     public class RepositoryMySql : IAuthenticate
     {
+        private IDataAccess idataAccess = null;
+
+        public RepositoryMySql()
+        {
+            idataAccess = GenericFactory<DataAccessMySql, IDataAccess>.CreateInstance();
+        }
+
         public bool ChangePassword(string userName, string password, string newPassword)
         {
-            throw new NotImplementedException();
+            bool bRes = false;
+            if (String.IsNullOrEmpty(userName))
+                throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (String.IsNullOrEmpty(password))
+                throw new ArgumentException("Value cannot be null or empty.", "password");
+            if (String.IsNullOrEmpty(newPassword))
+                throw new ArgumentException("Value cannot be null or empty.", "newPassword");
+
+            try
+            {
+                // user is already validated in AuthController.cs
+                // Hence we don't need to validate again
+                string sql = "update Users set Password=@newPassword where " +
+                     "Username=@userName and Password=@password";
+                List<DbParameter> PList = new List<DbParameter>();
+                DbParameter p1 = new MySqlParameter("@newPassword", MySqlDbType.VarChar, 50);
+                p1.Value = newPassword;
+                PList.Add(p1);
+                DbParameter p2 = new MySqlParameter("@userName", MySqlDbType.VarChar, 50);
+                p2.Value = userName;
+                PList.Add(p2);
+                DbParameter p3 = new MySqlParameter("@password", MySqlDbType.VarChar, 50);
+                p3.Value = password;
+                PList.Add(p3);
+                bRes = idataAccess.InsOrUpdOrDel(sql, PList) >= 1 ? true : false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return bRes;
         }
 
         public string GetRolesForUser(string uname)
         {
-            throw new NotImplementedException();
+            string roles = "";
+            string connStr = ConfigurationManager.ConnectionStrings["MYSQLDB"].ConnectionString;
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("GetUserRoles", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new MySqlParameter("@Username", uname));
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                    roles += reader["RoleName"].ToString() + "|";
+                if (roles != "")  // remove last "|"
+                    roles = roles.Substring(0, roles.Length - 1);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return roles;
         }
 
         public bool SignIn(string userName, string password, bool createPersistentCookie)
         {
-            throw new NotImplementedException();
+            bool bRes = false;
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+            try
+            {
+                if (ValidateUser(userName, password))
+                {
+                    string roles = GetRolesForUser(userName);
+
+                    FormsAuthenticationTicket authTicket     // cookie timeout is also set
+                        = new FormsAuthenticationTicket(1, userName, DateTime.Now, DateTime.Now.AddMinutes(5), false, roles);
+
+                    //  encrypt the ticket
+                    string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+
+                    // add the encrypted ticket to the cookie as data
+                    HttpCookie authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+
+                    System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
+
+                    bRes = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return bRes;
         }
 
         public void SignOut()
         {
-            throw new NotImplementedException();
+            FormsAuthentication.SignOut();
         }
 
         public bool ValidateUser(string userName, string password)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
+
+            object obj = null;
+            try
+            {
+                string sql = "select Username from Users where Username=@userName" +
+                    " and Password=@password";
+                List<DbParameter> PList = new List<DbParameter>();
+                DbParameter p1 = new MySqlParameter("@userName", MySqlDbType.VarChar, 50);
+                p1.Value = userName;
+                PList.Add(p1);
+                DbParameter p2 = new MySqlParameter("@password", MySqlDbType.VarChar, 50);
+                p2.Value = password;
+                PList.Add(p2);
+
+                obj = idataAccess.GetSingleAnswer(sql, PList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return obj != null ? true : false;
         }
     }
 }
--- Shopping/Data/RepositorySql.cs
+++ Shopping/Data/RepositorySql.cs
@@ -1,35 +1,155 @@
-﻿using System;
+﻿using Shopping.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace Shopping.Data
 {
     public class RepositorySql : IAuthenticate
     {
+        private IDataAccess idataAccess = null;
+
+        public RepositorySql()
+        {
+            idataAccess = GenericFactory<DataAccessSql, IDataAccess>.CreateInstance();
+        }
+
         public bool ChangePassword(string userName, string password, string newPassword)
         {
-            throw new NotImplementedException();
+            bool bRes = false;
+            if (String.IsNullOrEmpty(userName))
+                throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (String.IsNullOrEmpty(password))
+                throw new ArgumentException("Value cannot be null or empty.", "password");
+            if (String.IsNullOrEmpty(newPassword))
+                throw new ArgumentException("Value cannot be null or empty.", "newPassword");
+
+            try
+            {
+                // user is already validated in AuthController.cs
+                // Hence we don't need to validate again
+                string sql = "update Users set Password=@newPassword where " +
+                     "Username=@userName and Password=@password";
+                List<DbParameter> PList = new List<DbParameter>();
+                DbParameter p1 = new SqlParameter("@newPassword", SqlDbType.VarChar, 50);
+                p1.Value = newPassword;
+                PList.Add(p1);
+                DbParameter p2 = new SqlParameter("@userName", SqlDbType.VarChar, 50);
+                p2.Value = userName;
+                PList.Add(p2);
+                DbParameter p3 = new SqlParameter("@password", SqlDbType.VarChar, 50);
+                p3.Value = password;
+                PList.Add(p3);
+                bRes = idataAccess.InsOrUpdOrDel(sql, PList) >= 1 ? true : false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return bRes;
         }
 
         public string GetRolesForUser(string uname)
         {
-            throw new NotImplementedException();
+            string roles = "";
+            string connStr = ConfigurationManager.ConnectionStrings["SQLDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("GetUserRoles", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@Username", uname));
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                    roles += reader["RoleName"].ToString() + "|";
+                if (roles != "")  // remove last "|"
+                    roles = roles.Substring(0, roles.Length - 1);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return roles;
         }
 
         public bool SignIn(string userName, string password, bool createPersistentCookie)
         {
-            throw new NotImplementedException();
+            bool bRes = false;
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+            try
+            {
+                if (ValidateUser(userName, password))
+                {
+                    string roles = GetRolesForUser(userName);
+
+                    FormsAuthenticationTicket authTicket     // cookie timeout is also set
+                        = new FormsAuthenticationTicket(1, userName, DateTime.Now, DateTime.Now.AddMinutes(5), false, roles);
+
+                    //  encrypt the ticket
+                    string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+
+                    // add the encrypted ticket to the cookie as data
+                    HttpCookie authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+
+                    System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
+
+                    bRes = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return bRes;
         }
 
         public void SignOut()
         {
-            throw new NotImplementedException();
+            FormsAuthentication.SignOut();
         }
 
         public bool ValidateUser(string userName, string password)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(userName)) throw new ArgumentException("Value cannot be null or empty.", "userName");
+            if (String.IsNullOrEmpty(password)) throw new ArgumentException("Value cannot be null or empty.", "password");
+
+            object obj = null;
+            try
+            {
+                string sql = "select Username from Users where Username=@userName" +
+                    " and Password=@password";
+                List<DbParameter> PList = new List<DbParameter>();
+                DbParameter p1 = new SqlParameter("@userName", SqlDbType.VarChar, 50);
+                p1.Value = userName;
+                PList.Add(p1);
+                DbParameter p2 = new SqlParameter("@password", SqlDbType.VarChar, 50);
+                p2.Value = password;
+                PList.Add(p2);
+
+                obj = idataAccess.GetSingleAnswer(sql, PList);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return obj != null ? true : false;
         }
     }
 }
