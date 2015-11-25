5ea7ba4008071e5e6ffa611a60f16846056a001d
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-24 22:51:40 -0500

    Added generic factory class

---
+++ Shopping/Utilities/GenericFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Utilities
+{
+    public class GenericFactory<T, I>
+        where T : I
+    {
+        GenericFactory()
+        {
+
+        }
+
+        public static I CreateInstance(params object[] args)
+        {
+            return (I)Activator.CreateInstance(typeof(T), args);
+        }
+    }
+}
