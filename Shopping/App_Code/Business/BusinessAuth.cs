46b1e5d60c0473bcf2d6ccc3b19daa62fea4b18c
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-25 01:44:07 -0500

    implementation for business authentication

--- Shopping/App_Code/Business/BusinessAuth.cs
+++ Shopping/App_Code/Business/BusinessAuth.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shopping.Data;
+using Shopping.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,34 +9,37 @@ namespace Shopping.Business
 {
     public class BusinessAuth : IBusinessAuth
     {
+        private IAuthenticate iAuthData = null;
+
         public BusinessAuth()
         {
-
+            iAuthData = GenericFactory<RepositoryAbstraction, IAuthenticate>.CreateInstance();
         }
 
         #region IBusinessAuth members
         public string GetRolesForUser(string uname)
         {
-            return "";
+            return iAuthData.GetRolesForUser(uname);
         }
 
         public bool SignIn(string userName, string password, bool createPersistentCookie)
         {
-            return false;
+            return iAuthData.SignIn(userName, password, createPersistentCookie);
         }
 
         public bool ChangePassword(string userName, string password, string newPassword)
         {
-            return false;
+            return iAuthData.ChangePassword(userName, password, newPassword);
         }
 
         public void SignOut()
         {
+            iAuthData.SignOut();
         }
 
         public bool ValidateUser(string userName, string password)
         {
-            return false;
+            return iAuthData.ValidateUser(userName, password);
         }
         #endregion
     }
