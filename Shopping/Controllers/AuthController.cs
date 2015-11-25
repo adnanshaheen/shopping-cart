fdbdbe17bbe05c881a4be16892283c2807d243b7
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-24 19:26:53 -0500

    Login page's View, Model and Controller

---
+++ Shopping/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Shopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping.Controllers
+{
+    [HandleError]
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            LoginModel model = new LoginModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            return View(model);
+        }
+    }
+}
