e8acd02e15af7df734f26ff59fe0ce6993885dd4
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-24 22:44:39 -0500

    Add Repository authentication

---
+++ Shopping/Data/IAuthenticate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Data
+{
+    public interface IAuthenticate
+    {
+        string GetRolesForUser(string uname);
+        bool SignIn(string userName, string password, bool createPersistentCookie);
+        bool ChangePassword(string userName, string password, string newPassword);
+        void SignOut();
+        bool ValidateUser(string userName, string password);
+    }
+}
---
+++ Shopping/Data/RepositorySql.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Data
+{
+    public class RepositorySql : IAuthenticate
+    {
+        public bool ChangePassword(string userName, string password, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRolesForUser(string uname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SignIn(string userName, string password, bool createPersistentCookie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidateUser(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
