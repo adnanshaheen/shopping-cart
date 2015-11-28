8bb5c05607c275d6df73b85ca5789c45cb500f5c
Adnan Shaheen <shaheen4qau@gmail.com>
2015-11-27 22:59:06 -0500

    ProductsModel class, interface and repository helper added

---
+++ Shopping/Models/ProductModel.cs
@@ -0,0 +1,29 @@
+﻿using Shopping.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace Shopping.Models
+{
+    public class ProductModel : IEntity
+    {
+        public int CatagoryID { get; set; }
+        public string ShortDesc { get; set; }
+        public string LongDesc { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public int Inventory { get; set; }
+
+        public void SetFeilds(DataRow dataRow)
+        {
+            CatagoryID = (int)dataRow["CatID"];
+            ShortDesc = (string)dataRow["ProductSDesc"];
+            LongDesc = (string)dataRow["ProductLDesc"];
+            Image = (string)dataRow["ProductImage"];
+            Price = (double)dataRow["Price"];
+            Inventory = (int)dataRow["Inventory"];
+        }
+    }
+}
---
+++ Shopping/Utilities/RepositoryHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Shopping.Utilities
+{
+    public class RepositoryHelper
+    {
+        public RepositoryHelper()
+        {
+
+        }
+
+        public static List<T> ConvertToList<IEntity>(DataTable dataTable)
+            where T:IEntity, new()
+        {
+            List<T> TList = new List<T>();
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                T item = new T();
+                item.SetFeilds(dataRow);
+                TList.Add(item);
+            }
+
+            return TList;
+        }
+    }
+}
