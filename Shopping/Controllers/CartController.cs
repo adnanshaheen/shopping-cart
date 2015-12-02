8b7c8937c89dc5db46ad1e2a34b81c8034696b3f
Adnan Shaheen <shaheen4qau@gmail.com>
2015-12-01 23:37:35 -0500

    added cart view and controller

---
+++ Shopping/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using Shopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopping.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult ViewCart()
+        {
+            List<CartModel> Cart = null;
+            return View(Cart);
+        }
+    }
+}
