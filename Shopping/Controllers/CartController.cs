c86ff77699ec2e795dac6b85770536b51f5b8a44
Adnan Shaheen <shaheen4qau@gmail.com>
2015-12-05 23:09:33 -0500

    view cart response action added

--- Shopping/Controllers/CartController.cs
+++ Shopping/Controllers/CartController.cs
@@ -27,7 +27,6 @@ namespace Shopping.Controllers
             try
             {
                 cartCookieList = new List<CartModel>();
-                CartModel c1 = new CartModel();
                 CookieHelper<List<CartModel>>.GetValueFromCookie("cart", ref cartCookieList);
                 foreach (var item in cartCookieList)
                 {
@@ -50,19 +49,45 @@ namespace Shopping.Controllers
         [HttpPost]
         public ActionResult ViewCart(List<CartModel> model)
         {
+            // FIXME: model is empty
+            // either search for a way to return the whole list back from view
+            // or make another model class and have List<CartMode> as it's object
             if (Request.Form["btnClear"] != null)
             {
-
+                if (model.Any())
+                {
+                    CookieHelper<List<CartModel>>.SetValueToCookie("cart", model, DateTime.Now.AddDays(-1d));
+                    CookieHelper<List<CartModel>>.SetValueToCookie("oldcart", model, DateTime.MaxValue);
+                    model.Clear();
+                }
             }
             else if (Request.Form["btnUpdate"] != null)
             {
-
+                if (model.Any())
+                {
+                    CookieHelper<List<CartModel>>.SetValueToCookie("cart", model, DateTime.MaxValue);
+                    CookieHelper<List<CartModel>>.SetValueToCookie("oldcart", model, DateTime.MaxValue);
+                }
             }
             else if (Request.Form["btnCancel"] != null)
             {
-
+                CookieHelper<List<CartModel>>.GetValueFromCookie("oldcart", ref model);
+                if (model.Any())
+                {
+                    CookieHelper<List<CartModel>>.SetValueToCookie("oldcart", model, DateTime.Now.AddDays(-1d));
+                    CookieHelper<List<CartModel>>.SetValueToCookie("cart", model, DateTime.MaxValue);
+                    foreach (var item in model)
+                    {
+                        ProductModel product = iBusinessShop.GetProduct(item.ProductID.ToString());
+                        if (product != null)
+                        {
+                            item.ProductName = product.ShortDesc;
+                            item.ProductPrice = product.Price;
+                        }
+                    }
+                }
             }
-            return View();
+            return View(model);
         }
     }
 }
